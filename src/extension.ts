f1b023dc22ce3fc739917c0ab1510f01ea22e583
miyas <aabc.aabc.zx@gmail.com>
2026-03-15 01:03:40 +0900

    プラグインの対象範囲をコードブロックに変更 統合設定から置換対象と置換文字列を取得するようコード修正

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,26 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
+    // 初回インストール時のみ表示
+    const hasShownReadme = context.globalState.get('hasShownReadme', false);
+    if (!hasShownReadme) {
+        vscode.commands.executeCommand('extension.open', context.extension.id);
+        context.globalState.update('hasShownReadme', true);
+    }
+    //vscode統合設定読み込み
+    const config = vscode.workspace.getConfiguration('custom_gijiroku');
+    const enable = config.get<boolean>('enable');
+    const targetText = config.get<string>('targetText');
+    const replaceChar = config.get<string>('replaceChar');
+    const targetRegex = new RegExp(targetText || '', 'g');
     return {
         extendMarkdownIt(md: any) {
             // 自作の置換ルールを定義
             md.core.ruler.after('block', 'my_custom_replace', (state: any) => {
                 state.tokens.forEach((token: any) => {
-                    if (token.type !== 'inline') return;
-					    if (/\.\w+/.test(token.content)) {
-							console.log('Match found in:', token.content); // ログに出れば成功
-							token.content = token.content.replace(/\./g, '★ドットあり');
-						}
-
-                    token.children.forEach((child: any) => {
-                        if (child.type !== 'text') return;
-
-
-                    });
+                    if (token.type !== 'fence' || token.info.trim() !== 'giji') {return;}
+                    targetRegex.lastIndex = 0;
+                    token.content = token.content.replace(targetRegex, replaceChar);
                 });
             });
 
